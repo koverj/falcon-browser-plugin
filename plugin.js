04618c53d6cf14f5851f650a883dc82404e278f5
Alexey Panashchenko <alpa@ciklum.com>
2020-02-28 14:22:12 +0200

    Add mock_data for test plugin

--- plugin.js
+++ plugin.js
@@ -1,6 +1,8 @@
 var version = "1.3";
+// var json = require('./mock_locators.json');
 
 const BACKEND_URL = "http://localhost:8086/locators";
+const IS_MOCK = true;
 
 function getCurrentTabUrl() {
   chrome.tabs.query(
@@ -15,19 +17,34 @@ function getCurrentTabUrl() {
 }
 
 const getData = currentTabUrl => {
-  console.log(currentTabUrl);
+  if (!IS_MOCK) {
+    console.log(currentTabUrl);
 
-  const req = new XMLHttpRequest();
-  req.open("GET", `${BACKEND_URL}?url=${currentTabUrl}`, true);
-  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-  req.send();
+    const req = new XMLHttpRequest();
+    req.open("GET", `${BACKEND_URL}?url=${currentTabUrl}`, true);
+    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+    req.send();
 
-  req.onreadystatechange = function() {
-    // Call a function when the state changes.
-    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-      hightLightElements(JSON.parse(this.responseText));
-    }
-  };
+    req.onreadystatechange = function () {
+      // Call a function when the state changes.
+      if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+        hightLightElements(JSON.parse(this.responseText));
+      }
+    };
+  } else {
+    console.log(IS_MOCK)
+    chrome.runtime.getPackageDirectoryEntry(function(root) {
+      root.getFile("mock_locators.json", {}, function(fileEntry) {
+        fileEntry.file(function(file) {
+          var reader = new FileReader();
+          reader.onloadend = function(e) {
+            hightLightElements(JSON.parse(this.result));
+          };
+          reader.readAsText(file);
+        });
+      });
+    });
+  }
 };
 
 const hightLightElements = data => {
