735d65679fadb0a5617030f6858016709e074379
sergey <sergio_89@ukr.net>
2020-03-02 14:08:59 +0200

    add notify js

--- content.js
+++ content.js
@@ -18,13 +18,48 @@ const getLocatorsFromStorage = () => {
 };
 
 const addStyle = (locator, value) => {
+  $(locator).notify(value["tests"].length, {
+    // whether to hide the notification on click
+    clickToHide: false,
+    // whether to auto-hide the notification
+    autoHide: false,
+    // if autoHide, hide after milliseconds
+    autoHideDelay: 5000,
+    // show the arrow pointing at the element
+    arrowShow: true,
+    // arrow size in pixels
+    arrowSize: 5,
+    // position defines the notification position though uses the defaults below
+    // position: "right",
+    // default positions
+    elementPosition: "bottom right",
+    globalPosition: "top right",
+    // default style
+    style: "bootstrap",
+    // default class (string or [string])
+    className: "success",
+    // show animation
+    showAnimation: "slideDown",
+    // show animation duration
+    showDuration: 400,
+    // hide animation
+    hideAnimation: "slideUp",
+    // hide animation duration
+    hideDuration: 200,
+    // padding between element and notification
+    gap: 2
+  });
   const element = $(locator);
   if (!element) {
     return;
   }
 
+  // addPin(element, locator, value);
+};
+
+const addPin = (element, locator, value) => {
   element.addClass("label-relative");
-  if (element.children(".label").length > 1) {
+  if (element.children(".label").length >= 1) {
     return;
   }
 
