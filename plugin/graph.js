5a9afd1ba333109c1937c3056ae627ddcb52dd4d
sergey <sergio_89@ukr.net>
2020-03-20 11:29:53 +0200

    change velocity

--- plugin/graph.js
+++ plugin/graph.js
@@ -1,164 +1,166 @@
 chrome.storage.sync.get(["koverj_url", "activeBuild"], result => {
-    const req = new XMLHttpRequest();
-    req.open(
-        "GET",
-        `${result.koverj_url}/sitemap?buildId=${result.activeBuild}`,
-        true
-    );
-    req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
-    req.send();
+  const req = new XMLHttpRequest();
+  req.open(
+    "GET",
+    `${result.koverj_url}/sitemap?buildId=${result.activeBuild}`,
+    true
+  );
+  req.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+  req.send();
 
-    req.onreadystatechange = function () {
-        // Call a function when the state changes.
-        if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
-            const resp = JSON.parse(this.responseText);
+  req.onreadystatechange = function() {
+    // Call a function when the state changes.
+    if (this.readyState === XMLHttpRequest.DONE && this.status === 200) {
+      const resp = JSON.parse(this.responseText);
 
-            console.log(resp)
-            // // create an array with nodes
-            let nodes = new vis.DataSet(resp.nodes);
-            console.log('nodes', nodes);
-            //
-            // // create an array with edges
-            let edges = new vis.DataSet(resp.edges);
-            console.log('edges', edges);
+      console.log(resp);
+      // // create an array with nodes
+      let nodes = new vis.DataSet(resp.nodes);
+      console.log("nodes", nodes);
+      //
+      // // create an array with edges
+      let edges = new vis.DataSet(resp.edges);
+      console.log("edges", edges);
 
+      const nodeFilterSelector = document.getElementById("nodeFilterSelect");
+      const edgeFilters = document.getElementsByName("edgesFilter");
 
-            const nodeFilterSelector = document.getElementById("nodeFilterSelect");
-            const edgeFilters = document.getElementsByName("edgesFilter");
-
-            function startNetwork(data) {
-                const container = document.getElementById("mynetwork");
-                const options = {
-                    autoResize: true,
-                    nodes: {
-                        shape: "dot",
-                        size: 20,
-                        font: {
-                            size: 15,
-                            color: "black"
-                        },
-                        borderWidth: 1
-                    },
-                    edges: {
-                        width: 2
-                    },
-                    physics: {
-                        barnesHut: {gravitationalConstant: -50000, springLength: 100},
-                        stabilization: {iterations: 2500}
-                    },
-                    groups: {
-                        1: {color: "rgb(0,255,140)"},
-                        icons: {
-                            shape: "icon",
-                            icon: {
-                                face: "FontAwesome",
-                                code: "\uf0c0",
-                                size: 50,
-                                color: "orange"
-                            }
-                        },
-                        source: {
-                            color: {border: "white"}
-                        }
-                    },
-                };
-
-                new vis.Network(container, data, options);
+      function startNetwork(data) {
+        const container = document.getElementById("mynetwork");
+        const options = {
+          autoResize: true,
+          nodes: {
+            shape: "dot",
+            size: 20,
+            font: {
+              size: 15,
+              color: "black"
+            },
+            borderWidth: 1
+          },
+          edges: {
+            width: 2
+          },
+          physics: {
+            barnesHut: {
+              gravitationalConstant: -6000,
+              springLength: 100,
+              avoidOverlap: 1
+            },
+            stabilization: { iterations: 2500 },
+            maxVelocity: 0.9
+          },
+          groups: {
+            1: { color: "rgb(0,255,140)" },
+            icons: {
+              shape: "icon",
+              icon: {
+                face: "FontAwesome",
+                code: "\uf0c0",
+                size: 50,
+                color: "orange"
+              }
+            },
+            source: {
+              color: { border: "white" }
             }
+          }
+        };
 
-            /**
-             * filter values are updated in the outer scope.
-             * in order to apply filters to new values, DataView.refresh() should be called
-             */
-            let nodeFilterValue = "";
-            const edgesFilterValues = {};
+        new vis.Network(container, data, options);
+      }
 
-            let edgesArray = filteredEdges(resp.edges);
-            console.log('edgesArray', edgesArray);
-            createTestTitleNode(edgesArray, edgesFilterValues);
+      /**
+       * filter values are updated in the outer scope.
+       * in order to apply filters to new values, DataView.refresh() should be called
+       */
+      let nodeFilterValue = "";
+      const edgesFilterValues = {};
 
-            /*
+      let edgesArray = filteredEdges(resp.edges);
+      console.log("edgesArray", edgesArray);
+      createTestTitleNode(edgesArray, edgesFilterValues);
+
+      /*
                   filter function should return true or false
                   based on whether item in DataView satisfies a given condition.
                 */
-            const nodesFilter = node => {
-                if (nodeFilterValue === "") {
-                    return true;
-                }
-                // switch (nodeFilterValue) {
-                //     case "login":
-                //         return node.label === "https://angular.realworld.io/login";
-                //     case "register":
-                //         return node.label === "https://angular.realworld.io/register";
-                //     case "editor":
-                //         return node.label === "https://angular.realworld.io/editor";
-                //     case "settings":
-                //         return node.label === "https://angular.realworld.io/settings";
-                //     default:
-                //         return true;
-                // }
-            };
+      const nodesFilter = node => {
+        if (nodeFilterValue === "") {
+          return true;
+        }
+        // switch (nodeFilterValue) {
+        //     case "login":
+        //         return node.label === "https://angular.realworld.io/login";
+        //     case "register":
+        //         return node.label === "https://angular.realworld.io/register";
+        //     case "editor":
+        //         return node.label === "https://angular.realworld.io/editor";
+        //     case "settings":
+        //         return node.label === "https://angular.realworld.io/settings";
+        //     default:
+        //         return true;
+        // }
+      };
 
-            const edgesFilter = edge => {
-                return edgesFilterValues[edge.title];
-            };
+      const edgesFilter = edge => {
+        return edgesFilterValues[edge.title];
+      };
 
-            const nodesView = new vis.DataView(nodes, {filter: nodesFilter});
-            const edgesView = new vis.DataView(edges, {filter: edgesFilter});
+      const nodesView = new vis.DataView(nodes, { filter: nodesFilter });
+      const edgesView = new vis.DataView(edges, { filter: edgesFilter });
 
-            nodeFilterSelector.addEventListener("change", e => {
-                // set new value to filter variable
-                nodeFilterValue = e.target.value;
-                /*
+      nodeFilterSelector.addEventListener("change", e => {
+        // set new value to filter variable
+        nodeFilterValue = e.target.value;
+        /*
                       refresh DataView,
                       so that its filter function is re-calculated with the new variable
                     */
-                nodesView.refresh();
-            });
+        nodesView.refresh();
+      });
 
-            edgeFilters.forEach(filter =>
-                filter.addEventListener("change", e => {
-                    const {value, checked} = e.target;
-                    edgesFilterValues[value] = checked;
-                    edgesView.refresh();
-                })
-            );
+      edgeFilters.forEach(filter =>
+        filter.addEventListener("change", e => {
+          const { value, checked } = e.target;
+          edgesFilterValues[value] = checked;
+          edgesView.refresh();
+        })
+      );
 
-            startNetwork({nodes: nodesView, edges: edgesView});
-        }
-    };
-
-    function filteredEdges(edges) {
-        return edges.reduce((acc, current) => {
-            const x = acc.find(item => item.title === current.title);
-            if (!x) {
-                return acc.concat([current]);
-            } else {
-                return acc;
-            }
-        }, []);
-
+      startNetwork({ nodes: nodesView, edges: edgesView });
     }
+  };
 
-    function createTestTitleNode(edgesArray, edgesFilterValues) {
-        let testNames = document.getElementById("testNames");
+  function filteredEdges(edges) {
+    return edges.reduce((acc, current) => {
+      const x = acc.find(item => item.title === current.title);
+      if (!x) {
+        return acc.concat([current]);
+      } else {
+        return acc;
+      }
+    }, []);
+  }
 
-        for (let index in edgesArray) {
-            let edge = edgesArray[index];
-            console.log(edge);
-            let div = document.createElement('div');
-            let label = document.createElement('label');
-            let input = document.createElement('input');
-            input.setAttribute('type', 'checkbox');
-            input.setAttribute('name', 'edgesFilter');
-            input.setAttribute('value', edge.title);
-            label.appendChild(input);
-            let text = document.createTextNode(edge.title);
-            edgesFilterValues[edge.title] = false;
-            label.appendChild(text);
-            div.appendChild(label);
-            testNames.appendChild(div);
-        }
+  function createTestTitleNode(edgesArray, edgesFilterValues) {
+    let testNames = document.getElementById("testNames");
+
+    for (let index in edgesArray) {
+      let edge = edgesArray[index];
+      console.log(edge);
+      let div = document.createElement("div");
+      let label = document.createElement("label");
+      let input = document.createElement("input");
+      input.setAttribute("type", "checkbox");
+      input.setAttribute("name", "edgesFilter");
+      input.setAttribute("value", edge.title);
+      label.appendChild(input);
+      let text = document.createTextNode(edge.title);
+      edgesFilterValues[edge.title] = false;
+      label.appendChild(text);
+      div.appendChild(label);
+      testNames.appendChild(div);
     }
-
+  }
 });
